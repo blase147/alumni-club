9e09c0831e033015a42fbd7edc9177b42f9d6be4
blase147 <solarmails2@gmail.com>
2022-09-22 16:48:02 +0100

    Fix linters errors

--- js/appData.js
+++ js/appData.js
@@ -1,48 +1,52 @@
 const data = [
 
-    { id:0,
-      speakerImage: '../images/speaker_01.png',
-      speakerName:  'Chinonso Amadi',
-      description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laboru',
-      description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-    },
+  {
+    id: 0,
+    speakerImage: '../images/speaker_01.png',
+    speakerName: 'Chinonso Amadi',
+    description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laboru',
+    description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+  },
 
-    {   id:1,
-        speakerImage: '../images/speaker_02.png',
-        speakerName:  'Gozie Ekwuonu',
-        description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-        description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-      },
+  {
+    id: 1,
+    speakerImage: '../images/speaker_02.png',
+    speakerName: 'Gozie Ekwuonu',
+    description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+    description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+  },
 
-      { id:2,
-        speakerImage: '../images/speaker_03.png',
-        speakerName:  'Gozie Ekwuonu',
-        description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-        description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-      },
+  {
+    id: 2,
+    speakerImage: '../images/speaker_03.png',
+    speakerName: 'Gozie Ekwuonu',
+    description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+    description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+  },
 
-      { id:3,
-        speakerImage: '../images/speaker_04.png',
-        speakerName:  'Gozie Ekwuonu',
-        description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-        description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-      },
+  {
+    id: 3,
+    speakerImage: '../images/speaker_04.png',
+    speakerName: 'Gozie Ekwuonu',
+    description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+    description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+  },
 
-      { id:4,
-        speakerImage: '../images/speaker_05.png',
-        speakerName:  'Gozie Ekwuonu',
-        description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-        description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-      },
+  {
+    id: 4,
+    speakerImage: '../images/speaker_05.png',
+    speakerName: 'Gozie Ekwuonu',
+    description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+    description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+  },
 
-      { id:5,
-        speakerImage: '../images/speaker_06.png',
-        speakerName:  'Gozie Ekwuonu',
-        description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-        description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
-      },
+  {
+    id: 5,
+    speakerImage: '../images/speaker_06.png',
+    speakerName: 'Gozie Ekwuonu',
+    description1: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+    description2: 'Lorem ipsum dolor sit amet consectetur adipisicing elitModi, quos laborum',
+  },
 
-        
-  ];
-  export default data; // export default data;
-  
+];
+export default data; // export default data;
--- js/index.js
+++ js/index.js
@@ -6,26 +6,26 @@ const menu = document.getElementById('nav-items');
 const featuredSpeaker = document.getElementById('featured-speakers-card-container');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.add('menuShown');
-    closeBtn.style.display = 'block';
-  });
-  closeBtn.addEventListener('click', () => {
-    closeBtn.style.display = 'none';
-    menu.classList.remove('menuShown');
-  });
-  
-  menu.addEventListener('click', () => {
-    closeBtn.style.display = 'none';
-    menu.classList.remove('menuShown');
-  });
+  menu.classList.add('menuShown');
+  closeBtn.style.display = 'block';
+});
+closeBtn.addEventListener('click', () => {
+  closeBtn.style.display = 'none';
+  menu.classList.remove('menuShown');
+});
 
-  data.forEach((data) => {
-    const {
-      speakerImage, speakerName, description1, description2,
-    } = data;
-  
-    const speaker = document.createElement('div');
-    speaker.innerHTML += `
+menu.addEventListener('click', () => {
+  closeBtn.style.display = 'none';
+  menu.classList.remove('menuShown');
+});
+
+data.forEach((data) => {
+  const {
+    speakerImage, speakerName, description1, description2,
+  } = data;
+
+  const speaker = document.createElement('div');
+  speaker.innerHTML += `
     <div class="featured-speakers-card">
             <img  class="featured-speakers-card-image" src="${speakerImage}" alt="Speaker Image">
     <div class="featured-speakers-card-content">
@@ -40,23 +40,5 @@ menuBtn.addEventListener('click', () => {
     </div>
   </div>
 `;
-    featuredSpeaker.appendChild(speaker);
-})
-
-function viewMore() {
-  var fsData2 = document.getElementById('featured-speakers-card ${data #2 #3 #4 #5}');
-  var fSpeaker = document.getElementById('featured-speakers-card ${#0 #1}');
-  var moreButton = document.getElementById('more');
-
-
-  if (fsData2.style.display === "block") {
-    fsData2.style.display = "none";
-    fSpeaker.style.display = "block";
-  } else {
-    fsData2.style.display = "block";
-    moreButton.innerHTML = "less"; 
-    fSpeaker.style.display = "none";
-  }
-}
-
-
+  featuredSpeaker.appendChild(speaker);
+});
