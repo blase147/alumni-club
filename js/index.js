48b5e6fbd4e6aa5c6fcc7deba2c911b9b29ae3b5
blase147 <solarmails2@gmail.com>
2022-09-23 06:48:23 +0100

    Fix linters error

--- js/index.js
+++ js/index.js
@@ -42,3 +42,69 @@ data.forEach((data) => {
 `;
   featuredSpeaker.appendChild(speaker);
 });
+
+const form = document.querySelector('#form');
+const messageContainer = document.querySelector('.message');
+
+const isValidEmail = (email) => {
+  const eamilPattern = /^(([^<>()[\]\\.,;:\s@"A-Z]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+  return eamilPattern.test(email);
+};
+
+form.addEventListener('submit', (e) => {
+  const formData = new FormData(e.target);
+  const valid = isValidEmail(formData.get('email'));
+  if (valid) {
+    const message = document.createElement('span');
+    message.classList.add('success');
+    message.innerHTML = 'Data sent successfully!';
+    messageContainer.replaceChildren(message);
+  } else {
+    e.preventDefault();
+    const message = document.createElement('span');
+    message.classList.add('error');
+    message.innerHTML = 'Incorrect format. Enter email in lowercase';
+    messageContainer.replaceChildren(message);
+  }
+});
+
+// Mobile form Local Storage
+const name = form.elements.item(0);
+const email = form.elements.item(1);
+const comment = form.elements.item(2);
+function saveData() {
+  const data = {
+    Name: form.elements.item(0).value,
+    Email: form.elements.item(1).value,
+    Comment: form.elements.item(2).value,
+  };
+  localStorage.setItem('data', JSON.stringify(data));
+}
+
+let formObject = JSON.parse(localStorage.getItem('data'));
+if (!formObject) {
+  formObject = {
+    name: '',
+    email: '',
+    comment: '',
+  };
+  saveData();
+}
+
+name.value = formObject.name;
+name.addEventListener('change', (e) => {
+  formObject.name = e.target.value;
+  localStorage.setItem('data', JSON.stringify(formObject));
+});
+
+email.value = formObject.email;
+email.addEventListener('change', (e) => {
+  formObject.email = e.target.value;
+  localStorage.setItem('data', JSON.stringify(formObject));
+});
+
+comment.value = formObject.comment;
+comment.addEventListener('change', (e) => {
+  formObject.comment = e.target.value;
+  localStorage.setItem('data', JSON.stringify(formObject));
+});
